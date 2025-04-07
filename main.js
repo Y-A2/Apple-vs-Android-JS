1f992bb602af818bea3eef0da46d90c262541d49
Y-A2 <y.ahmed9@share.epsb.ca>
2025-04-07 13:51:56 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+// Changing Attributes in JS vs Android
+
+document.getElementById("button1").addEventListener("click", showAndroid);
+document.getElementById("button2").addEventListener("click", showApple);
+
+
+function showAndroid (){
+    document.getElementById("logo").src = "img/Android-Logo.jpg";
+    document.getElementById("explore").innerHTML = "Android Home";
+    document.getElementById("explore").href = "https://android.com";
+    document.getElementById("explore").style.background = "#a4c93b";
+    document.getElementById("page").style.background = "#a4c93b";
+    //font family
+    document.getElementById("button1").classList.add("active")
+    document.getElementById("button2").classList.remove("active")
+}
+
+function showApple (){
+    document.getElementById("logo").src = "img/Apple-Logo.jpg";
+    document.getElementById("explore").innerHTML = "Apple Home";
+    document.getElementById("explore").style.background = "#b6bcca";
+    document.getElementById("explore").href = "https://www.apple.com/&#39";
+    document.getElementById("page").style.background = "#b6bcca";
+    document.getElementById("button1").classList.remove("active");
+    document.getElementById("font").style.fontFamily = "cursive"
+
+}
